c0c606ea8daa46a0b4bee3d355024ef6d929b049
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2021-01-14 18:04:32 -0800

    feat: added a click listener for when the button is pressed to show a toast message

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.rollButton.text = "Let's Roll"
+        //binding.rollButton.text = "Let's Roll"
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
